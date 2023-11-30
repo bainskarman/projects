131b84817c73642b7ef9035013512bcc6c2b5ea6
Karman Singh Bains <122693789+bainskarman@users.noreply.github.com>
2023-11-30 18:08:26 +0000

    app

--- IPL Winner Prediction/app.py
+++ IPL Winner Prediction/app.py
@@ -1,65 +1,68 @@
 import streamlit as st
-import pickle
+import joblib
 import pandas as pd
 
 teams = ['Rajasthan Royals',
- 'Royal Challengers Bangalore',
- 'Sunrisers Hyderabad',
- 'Delhi Capitals',
- 'Chennai Super Kings',
- 'Gujarat Titans',
- 'Lucknow Super Giants',
- 'Kolkata Knight Riders',
- 'Punjab Kings',
- 'Mumbai Indians']
+    'Royal Challengers Bangalore',
+    'Sunrisers Hyderabad',
+    'Delhi Capitals',
+    'Chennai Super Kings',
+    'Gujarat Titans',
+    'Lucknow Super Giants',
+    'Kolkata Knight Riders',
+    'Punjab Kings',
+    'Mumbai Indians']
 
 cities = ['Ahmedabad',
- 'Kolkata',
- 'Mumbai',
- 'Navi Mumbai',
- 'Pune',
- 'Dubai',
- 'Sharjah',
- 'Abu Dhabi',
- 'Delhi',
- 'Chennai',
- 'Hyderabad',
- 'Visakhapatnam',
- 'Chandigarh',
- 'Bengaluru',
- 'Jaipur',
- 'Indore',
- 'Bangalore',
- 'Raipur',
- 'Ranchi',
- 'Cuttack',
- 'Dharamsala',
- 'Nagpur',
- 'Johannesburg',
- 'Centurion',
- 'Durban',
- 'Bloemfontein',
- 'Port Elizabeth',
- 'Kimberley',
- 'East London',
- 'Cape Town']
+    'Kolkata',
+    'Mumbai',
+    'Navi Mumbai',
+    'Pune',
+    'Dubai',
+    'Sharjah',
+    'Abu Dhabi',
+    'Delhi',
+    'Chennai',
+    'Hyderabad',
+    'Visakhapatnam',
+    'Chandigarh',
+    'Bengaluru',
+    'Jaipur',
+    'Indore',
+    'Bangalore',
+    'Raipur',
+    'Ranchi',
+    'Cuttack',
+    'Dharamsala',
+    'Nagpur',
+    'Johannesburg',
+    'Centurion',
+    'Durban',
+    'Bloemfontein',
+    'Port Elizabeth',
+    'Kimberley',
+    'East London',
+    'Cape Town']
 
-with open('pipeline.pkl', 'rb') as file:
-    pipeline = pickle.load(file)
+file_path='/workspaces/projects/IPL Winner Prediction/my_pipeline.joblib'
+with open(file_path, 'rb') as file:
+    pipe = joblib.load(file)
+
 st.title('IPL Win Predictor')
 
-col1, col2 = st.beta_columns(2)
+# Using st.columns instead of st.beta_columns
+col1, col2 = st.columns(2)
 
 with col1:
-    batting_team = st.selectbox('Select the batting team',sorted(teams))
+    batting_team = st.selectbox('Select the batting team', sorted(teams))
 with col2:
-    bowling_team = st.selectbox('Select the bowling team',sorted(teams))
+    bowling_team = st.selectbox('Select the bowling team', sorted(teams))
 
-selected_city = st.selectbox('Select host city',sorted(cities))
+selected_city = st.selectbox('Select host city', sorted(cities))
 
 target = st.number_input('Target')
 
-col3,col4,col5 = st.beta_columns(3)
+col3, col4, col5 = st.columns(3)
 
 with col3:
     score = st.number_input('Score')
@@ -70,15 +73,17 @@ with col5:
 
 if st.button('Predict Probability'):
     runs_left = target - score
-    balls_left = 120 - (overs*6)
+    balls_left = 120 - (overs * 6)
     wickets = 10 - wickets
-    crr = score/overs
-    rrr = (runs_left*6)/balls_left
+    crr = score / overs
+    rrr = (runs_left * 6) / balls_left
 
-    input_df = pd.DataFrame({'BattingTeam':[batting_team],'Team2':[bowling_team],'City':[selected_city],'runs_left':[runs_left],'balls_left':[balls_left],'wickets_left':[wickets],'total_runs':[target],'crr':[crr],'rrr':[rrr]})
+    input_df = pd.DataFrame({'BattingTeam': [batting_team], 'Team2': [bowling_team], 'City': [selected_city],
+                             'runs_left': [runs_left], 'balls_left': [balls_left], 'wickets_left': [wickets],
+                             'team1_runs': [target], 'crr': [crr], 'rrr': [rrr]})
 
-    result = pipeline.predict_proba(input_df)
+    result = pipe.predict_proba(input_df)
     loss = result[0][0]
     win = result[0][1]
-    st.header(batting_team + "- " + str(round(win*100)) + "%")
-    st.header(bowling_team + "- " + str(round(loss*100)) + "%")
+    st.header(batting_team + "- " + str(round(win * 100)) + "%")
+    st.header(bowling_team + "- " + str(round(loss * 100)) + "%")
