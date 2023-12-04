10e8e3ab4a96edfa654f0444f38d596641284cc5
Karman Singh Bains <122693789+bainskarman@users.noreply.github.com>
2023-12-04 11:44:49 +0000

    app

---
+++ Stock_predict/app.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns 
+import streamlit as st
+from keras.models import load_model
+import yfinance as yf
+
+st.title('Stock Trend Prediction')
+user_input=st.text_input('Enter stock ticker')
+df = yf.download(user_input,start='2010-01-01')
+st.subheader('Data after 2010')
+from sklearn.preprocessing import MinMaxScaler
+scaler=MinMaxScaler(feature_range=(0,1))
+df1=scaler.fit_transform(df1)
+df1.shape
+training_size=int(len(df1)*0.65)
+test_size=len(df1)-training_size
+train_data,test_data=df1[0:training_size,:],df1[training_size:len(df1),:1]
+def create_dataset(dataset, time_step=1):
+	dataX, dataY = [], []
+	for i in range(len(dataset)-time_step-1):
+		a = dataset[i:(i+time_step), 0]   ###i=0, 0,1,2,3-----99   100 
+		dataX.append(a)
+		dataY.append(dataset[i + time_step, 0])
+	return numpy.array(dataX), numpy.array(dataY)
+time_step = 100
+X_train, y_train = create_dataset(train_data, time_step)
+X_train =X_train.reshape(X_train.shape[0],X_train.shape[1] , 1)
+
+model = load_model('/workspaces/projects/Stock_predict/lmodel.h5')
+
+past_100 = train_data.tail(100)
+final_df= past_100.append(test_data,ignore_index=True)
+
+X_test, y_test = create_dataset(final_df, time_step)
+X_test = X_test.reshape(X_test.shape[0],X_test.shape[1] , 1)
--- credit_score/app.py
+++ credit_score/app.py
@@ -27,42 +27,32 @@ behaviour = ['High_spent_Small_value_payments',
 
 category = ['Good', 'Standard', 'Poor']
 
-file_path='/workspaces/projects/Credit Score Classification/pipeline.joblib'
+file_path='/workspaces/projects/credit_score/pipeline.joblib'
 with open(file_path, 'rb') as file:
     pipe = joblib.load(file)
 
-col1, col2 = st.columns(2)
+col1, col2,col21 = st.columns(3)
 
 with col1:
     occupation = st.selectbox('Select the your Occupation', sorted(occupation))
 with col2:
-    behaviour = st.selectbox('Select spending behavious similar to yours', sorted(behaviour))
-
-col3, col4, col5, col6 = st.columns(4)
-
-with col3:
-    age = st.number_input('Age')
-with col4:
     salary = st.number_input('Monthly Inhand Salary')
+with col21:
+    cha= st.number_input('Credit History Age')
+
+col5, col6= st.columns(2)
+
 with col5:
     credit_cards = st.number_input('Number of Credit Cards')
 with col6:
-    interest_rate=st.number_input('Credit Card Interest Rate')
-
-col7, col8, col9 = st.columns(3)
-with col3:
     loans = st.number_input('Number of Loans')
-with col4:
+
+col7, col8= st.columns(2)  
+with col7:
     delayed_pay = st.number_input('Number of Delayed Payments')
 with col5:
     debt = st.number_input('Pending Debt Amount')
 
-col10, col11 = st.columns(2)
-
-with col10:
-    emi = st.number_input('Total EMI Cost per Month')
-with col11:
-    invest = st.number_input('Total Investment per Month')
 
 col12, col13 = st.columns(2)
 with col12:
@@ -72,21 +62,15 @@ with col13:
 
 if st.button('Predict CRS'):
     cur = (sp/cl)*100
-
     input_df = pd.DataFrame({
-    'Age': [age],
     'Occupation': [occupation],
     'Monthly_Inhand_Salary': [salary],
     'Num_Credit_Card': [credit_cards],
-    'Interest_Rate': [interest_rate],
     'Num_of_Loan': [loans],
     'Num_of_Delayed_Payment': [delayed_pay],
     'Outstanding_Debt': [debt],
     'Credit_Utilization_Ratio': [cur],
-    'Total_EMI_per_month': [emi],
-    'Amount_invested_monthly': [invest],
-    'Payment_Behaviour': [behaviour],
-    'Credit_Score': [credit_score]
+    'Credit_History_Age':[cha]
 })
     predicted_category = pipe.predict(input_df)[0]
     st.success(f'The predicted credit score category is: {predicted_category}')
