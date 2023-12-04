a0ce951b122dc623c99c3eea948036a0419e9968
Karman Singh Bains <122693789+bainskarman@users.noreply.github.com>
2023-12-04 12:55:18 +0000

    final 3 apps

--- Stock_predict/app.py
+++ Stock_predict/app.py
@@ -7,42 +7,45 @@ from keras.models import load_model
 import yfinance as yf
 
 st.title('Stock Trend Prediction')
-user_input=st.text_input('Enter stock ticker')
-df = yf.download(user_input,start='2010-01-01')
-st.subheader('Data after 2010')
+user_input=st.text_input('Enter stock ticker','AAPL')
+df = yf.download(user_input,start='2016-01-01')
+df = df.reset_index()
+st.subheader('Data after 2016')
+df1=pd.DataFrame(df['Close'])
+
 from sklearn.preprocessing import MinMaxScaler
 scaler=MinMaxScaler(feature_range=(0,1))
 df1=scaler.fit_transform(df1)
-df1.shape
-training_size=int(len(df1)*0.65)
-test_size=len(df1)-training_size
-train_data,test_data=df1[0:training_size,:],df1[training_size:len(df1),:1]
-def create_dataset(dataset, time_step=1):
+
+def split(dataset):
+	time_step=120
 	dataX, dataY = [], []
 	for i in range(len(dataset)-time_step-1):
 		a = dataset[i:(i+time_step), 0]   ###i=0, 0,1,2,3-----99   100 
 		dataX.append(a)
 		dataY.append(dataset[i + time_step, 0])
-	return numpy.array(dataX), numpy.array(dataY)
-time_step = 100
-X_train, y_train = create_dataset(train_data, time_step)
+	return np.array(dataX), np.array(dataY)
+
+X_train,y_train=split(df1)
+X_train=np.array(X_train)
+y_train=np.array(y_train)
 X_train =X_train.reshape(X_train.shape[0],X_train.shape[1] , 1)
 
 model = load_model('/workspaces/projects/Stock_predict/lmodel.h5')
 
-past_100 = train_data.tail(100)
-final_df= past_100.append(test_data,ignore_index=True)
+y_pred= model.predict(X_train)
 
-X_test, y_test = create_dataset(final_df, time_step)
-X_test = X_test.reshape(X_test.shape[0],X_test.shape[1] , 1)
-y_pred= model.predict()
-y_test=scaler.inverse_transform(y_test)
 y_pred=scaler.inverse_transform(y_pred)
+
+look_back=120
+trainPredictPlot = np.empty_like(df[['Close']])
+trainPredictPlot[:, :] = np.nan
+trainPredictPlot[look_back:len(y_pred)+look_back, :] = y_pred
 
 st.subheader('Original vs Predicted Price')
 fig1= plt.figure(figsize=(12,8))
-plt.plot(y_test,label='Original Price')
-plt.plot(y_pred,label='Original Price')
+plt.plot(trainPredictPlot,label='Predicted Price')
+plt.plot(df[['Close']],label='Original Price')
 plt.xlabel('Time')
 plt.ylabel('Price')
 plt.legend()
