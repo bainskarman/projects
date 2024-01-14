64b2ecc0d214233593796905496b8a6a14f5c63b
Karman Singh Bains <122693789+bainskarman@users.noreply.github.com>
2024-01-14 08:01:56 +0000

    final

--- Credit_Classification_End_to_End/Apps/newapp.py
+++ Credit_Classification_End_to_End/Apps/newapp.py
@@ -17,7 +17,12 @@ st.set_page_config(page_title='Credit Classification', layout='wide',initial_sid
                         'Report a bug': 'https://github.com/bainskarman/projects/issues',
                         'About': '''Enter the following information to get your credit score for previous 12 months or select a profile from the given options. This is a mock-up intended for information only, if you wish to learn more about the model behind this please go to the GitHub [Credit Analysis](github.com/bainskarman/projects/Credit_Classification_End_to_End)''' })
 
-model = joblib.load('/workspaces/projects/Credit_Classification_End_to_End/Apps/model.pkl')
+model_path = 'Apps/model.pkl'
+
+if os.path.exists(model_path):
+    model = joblib.load(model_path)
+else:
+    print(f"Error: Model file not found at {model_path}")
 
 st.title('Credit Score Analysis')
 
